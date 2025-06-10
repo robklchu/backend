b91a7e2a7aa6d0b8a17efe43086a6e47da4108a9
Black Lab <162184251+blacklabp@users.noreply.github.com>
2025-06-10 13:25:18 +0100

    part3 interim test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const Person = require("./models/person");
 
 const app = express();
-app.use(express.static('dist')); // to serve static files in 'dist'
+app.use(express.static("dist")); // to serve static files in 'dist'
 
 morgan.token("data", function (req, res) {
   return JSON.stringify(req.body);
@@ -15,26 +17,26 @@ app.use(
 app.use(express.json());
 
 let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+  // {
+  //   id: "1",
+  //   name: "Arto Hellas",
+  //   number: "040-123456",
+  // },
+  // {
+  //   id: "2",
+  //   name: "Ada Lovelace",
+  //   number: "39-44-5323523",
+  // },
+  // {
+  //   id: "3",
+  //   name: "Dan Abramov",
+  //   number: "12-43-234345",
+  // },
+  // {
+  //   id: "4",
+  //   name: "Mary Poppendieck",
+  //   number: "39-23-6423122",
+  // },
 ];
 
 app.get("/", (req, res) => {
@@ -49,18 +51,19 @@ app.get("/info", (req, res) => {
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => {
+      res.status(404).end();
+    });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -77,21 +80,24 @@ app.post("/api/persons", (req, res) => {
     return res.status(400).json({ error: "name or number is missing" });
   }
 
+  ///////////////////////////////
+  // To be updated
   if (persons.find((p) => p.name === body.name)) {
     return res.status(400).json({ error: "name must be unique" });
   }
+  ///////////////////////////////
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: String(Math.floor(Math.random() * 1e6)),
-  };
+  });
 
-  persons.push(person);
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001; // allow for external hosting
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
